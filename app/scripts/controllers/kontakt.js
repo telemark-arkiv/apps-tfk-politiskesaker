5ddca17cca6b3156562a6ee58162342575976f0d
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-01-23 09:01:07 +0100

    Updates kontakt.js

--- app/scripts/controllers/kontakt.js
+++ app/scripts/controllers/kontakt.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('appsTfkPolitiskesakerApp')
-  .controller('KontaktCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('KontaktCtrl', function ($scope, $http) {
+    $scope.results = [];
+
+    $http({'method': 'GET', 'url': 'data/kontakt.json'}).
+      success(function (data) {
+        $scope.results = data;
+      });
   });
