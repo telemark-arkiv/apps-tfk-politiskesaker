4ad3a7e214d7737f8b575f7891fe82d341994bde
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-01-23 08:24:39 +0100

    Adds real call to moter.js

--- app/scripts/controllers/moter.js
+++ app/scripts/controllers/moter.js
@@ -6,6 +6,20 @@ angular.module('appsTfkPolitiskesakerApp')
     var
       moteid = $routeParams.moteid;
 
-    $scope.mote = Api.getMotet(moteid);
+    $scope.mote = '';
+
+
+    Api.getMotet(moteid).
+      success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.mote = data.results;
+        $scope.loading = false;
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        $scope.loading = false;
+      });
 
   });
