42b75593fe84d3b851afbd72f571048c53aa4450
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-01-29 12:38:25 +0100

    Updates test

--- test/spec/directives/kontakttabell.js
+++ test/spec/directives/kontakttabell.js
@@ -15,6 +15,6 @@ describe('Directive: kontakttabell', function () {
   it('should make hidden element visible', inject(function ($compile) {
     element = angular.element('<kontakttabell></kontakttabell>');
     element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the kontakttabell directive');
+    expect(element.length).toBe(1);
   }));
 });
